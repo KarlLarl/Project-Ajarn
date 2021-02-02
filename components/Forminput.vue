19e9cb8916431ec7e62719c2a2f886d59c8482b1
Meen <peerapolmuadmu@gmail.com>
2021-02-02 21:26:05 +0700

    up

--- components/Forminput.vue
+++ components/Forminput.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div>
       <div><input placeholder="Email" type="text"></div>
@@ -16,23 +15,4 @@ input {
   font-weight: bold;
 
 }
-=======
-<template>
-  <div>
-      <div><input placeholder="Email" type="text"></div>
-      <div><input placeholder="Password" type="password"></div>
-  </div>
-</template>
-<style lang="scss" scoped>
-input {
-  width: 100%;
-  padding: 12px 20px;
-  border: none;
-  background-color: #F5EBEB;
-  border-radius: 10px;
-  margin-top: 10px;
-  font-weight: bold;
-
-}
->>>>>>> 4eaf842a4860f3e7af97562999f6b4fb5fac0763
 </style>
