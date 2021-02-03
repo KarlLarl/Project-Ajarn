2134c403eccb3d56d3248321b4db1464b3fea45c
Meen <peerapolmuadmu@gmail.com>
2021-02-04 00:50:03 +0700

    up

--- components/card.vue
+++ components/card.vue
@@ -1,10 +1,48 @@
 <template>
     <div class="back">
         <div class="card">
+            <!-- หัวข้อด้านบน -->
             <div class="card_top">
-                <div>หัวข้อโปรเจค</div>
-                <div>10/10/10</div>
-                <div><button></button></div>
+                <div class="txt_top">
+                    <div><h4>หัวข้อโปรเจค</h4></div>
+                    <div><h6>วันเวลา</h6></div>
+                </div>
+                <div><button class="btn-card-top"></button></div>
+            </div>
+            <!-- เนื้อหา Topic คืบหน้า -->
+            <div class="all_small">
+                <div class="small_topic_card" style="margin-top:10px">
+                    <div class="topic_small">
+                        Requirments
+                    </div>
+                    <div style="margin-top:20px">
+                        วันนัดหมาย
+                    </div>
+                </div>
+
+                <div class="small_topic_card" style="margin-top:10px">
+                    <div class="topic_small">
+                        Requirments
+                    </div>
+                    <div style="margin-top:20px">
+                        วันนัดหมาย
+                    </div>
+                </div>
+
+                <div class="small_topic_card" style="margin-top:10px">
+                    <div class="topic_small">
+                        Requirments
+                    </div>
+                    <div style="margin-top:20px">
+                        วันนัดหมาย
+                    </div>
+                </div>
+
+            </div>
+            <!-- btn ด้านล่าง -->
+            <div class="card_down">
+                <button>add topic card</button>
+                <button>GitHub</button>
             </div>
         </div>
     </div>
@@ -19,6 +57,9 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+h6{
+    margin-left: 5px;
+}
 .back{
     background: chartreuse;
     padding: 10px;
@@ -26,12 +67,40 @@ export default {
         background: #DCD7D7;
         padding: 10px;
         width: fit-content;
+        border-radius: 10px;
+        height: 300px;
         .card_top{
             display: flex;
-            .card_top button{
-                height: 100%;
-                background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Edit_icon_%28the_Noun_Project_30184%29.svg/1024px-Edit_icon_%28the_Noun_Project_30184%29.svg.png");
+            justify-content: space-between;
+            .txt_top{
+                display: flex;
+                align-items: baseline;
             }
+            .btn-card-top{
+                height: 100%;
+                background-image: url("https://image.flaticon.com/icons/png/512/84/84380.png");
+                background-size: cover;
+                border: none;
+                width: 21px;
+            }
+        }
+        .all_small{
+            margin-top: 10px;
+            height: 220px;
+            .small_topic_card{
+            background: white;
+            width: 230px;
+            border-radius: 10px;
+            padding: 5px;
+            font-weight: bold;
+            } 
+        }
+
+        .card_down{
+            display: flex;
+            justify-content: space-between;
+            align-items: baseline;
+            margin-top: 10px;
         }
     }
 }
